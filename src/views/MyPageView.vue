86b803cfc539286eee36d0b1b819b8865b42500d
KHH <gygus7345@naver.com>
2023-01-02 16:48:06 +0900

    마이페이지 수정

--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -13,23 +13,43 @@
           <v-app-bar-title>
             Email
           </v-app-bar-title>
-          <v-text-field style="width:50%" label="useing Email. this is readonly. if you click this component, Nothing is happening" value="asdf" readonly></v-text-field>
+          <v-text-field style="width:50%" :model="user.email" label="useing Email. this is readonly. if you click this component, Nothing is happening" value="asdf" readonly></v-text-field>
           <v-app-bar-title>
             password
           </v-app-bar-title>
-          <v-text-field style="width:50%" label="if you want change password, input new password" value=""></v-text-field>
+          <v-text-field
+            :model="user.password"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            :rules="pw_rules"
+            label="if you want change password,input new password"
+            counter
+            @click:append="show1 = !show1"
+            style="width:50%"
+          ></v-text-field>
           <v-app-bar-title>
             RePassword
           </v-app-bar-title>
-          <v-text-field style="width:50%" label="if you want change password, again input new password" value=""></v-text-field>
+          <v-text-field
+            :model="repassword"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            :rules="pw_rules"
+            label="if you want change password,input new password"
+            counter
+            @click:append="show1 = !show1"
+            style="width:50%"
+          ></v-text-field>
           <v-app-bar-title>
             name
           </v-app-bar-title>
-          <v-text-field style="width:50%" label="if you want change name, input please" value="이름"></v-text-field>
+          <v-text-field style="width:50%" :model="user.name" label="if you want change name, input please" value="이름"></v-text-field>
           <v-app-bar-title>
             phone
           </v-app-bar-title>
-          <v-text-field style="width:50%" label="if you want change phone, input please" value="폰번"></v-text-field>
+          <v-text-field style="width:50%" :model="user.phone" label="if you want change phone, input please" value="폰번"></v-text-field>
           <v-btn @click="save">저장</v-btn>
         </v-sheet>
       </v-col>
@@ -38,40 +58,58 @@
 
 <script>
 const axios = require('axios');
+const validatePassword = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/
+const validateEmail = /^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\.[A-Za-z0-9\\-]+/
 export default {
   created () {
     const userEmail = this.$store.getters.getUserEmail;
-    this.email= userEmail;
     axios({
     method: "POST",
     url: '/api/boardSave',
     data: {
-        email : this.email,
+        email : userEmail,
     },
     headers: {'Content-type': 'application/json'}
     }).then((res)=>{
         if(res.data.result === 200){
           this.user = res.data.user
         }else{
-            alert("문제가 생겼으니 관리자에게 문의하세요")
+           // alert("문제가 생겼으니 관리자에게 문의하세요")
         }
     }).catch(error=>{
-        alert("문제가 생겼으니 관리자에게 문의하세요")
-          });
+        //alert("문제가 생겼으니 관리자에게 문의하세요")
+    });
   },
   data() {
     return {
-      email: '',
-      user: {}
+      user: {},
+      repassword:'',
+      show1: false,
+      pw_rules: [
+        v => validatePassword.test(v) || '특수문자 / 문자 / 숫자 포함 형태의 8~16자리 이내의 암호 정규식을 지켜주세요.',
+      ],
     }
   },
   methods: {
     save() {
+      if(this.user.password != '' && !(validatePassword.test(this.user.password))){
+        alert("비밀번호 형식을 정확히 입력해주세요");
+        return false;
+      }
+      if(this.repassword != '' && !(validatePassword.test(this.repassword))){
+        alert("재입력한 비밀번호 형식을 정확히 입력해주세요");
+        return false;
+      }
+      if(this.user.password != this.repassword){
+        alert("비밀번호가 같지 않습니다!")
+        return false;
+      }
+
       axios({
         method: "POST",
         url: '/api/mypageSave',
         data: {
-            email : this.email,
+            user: this.user,
         },
         headers: {'Content-type': 'application/json'}
         }).then((res)=>{
