5e1f0c20104e292d5935cf5a35726fd6d0f126d7
Taeparu <gjsdlfkddmsd@gmail.com>
2023-01-20 13:13:31 +0900

    CommentView 왜안돼(중간저장)

--- src/components/replyComponent.vue
+++ src/components/replyComponent.vue
@@ -2,7 +2,14 @@
       <v-sheet
   width="100%"
   rounded="lg"
-  >     <div style="border: 1px solid; border-top:5px;">
+  >   
+  <v-container>
+    <v-content sytle="min-height: 100px;">
+      {{ comment.uid }}. {{ comment.created_user }}
+    {{ comment.comment }} {{ comment.created_at }}
+    </v-content>
+  </v-container>  
+  <div style="border: 1px solid; border-top:5px;">
         <ckeditor 
           v-model="comment" 
           height="10"
@@ -24,6 +31,32 @@
     created (){
       this.email = this.$store.getters.getUserEmail
       this.uid = this.$route.params.uid
+      axios({
+        method:"POST",
+        url:'/api/commentView/' + uid,
+        data:{
+          uid : this.uid,
+        },
+        headers : {'Content-type' : 'application/json'}
+      }).then((res)=>{
+        if(res.data.result === 200){
+          this.commentList = res.data.commentList;
+        }else if(res.data.result === 400){
+          alert("서버에 문제가 생겼으니 관리자에게 문의하세요")
+        }
+        else if(res.data.result === 500){
+          alert("서버에 문제가 생겼으니 관리자에게 문의하세요")
+        }
+      }).catch(error=>{
+           alert("문제가 생겼으니 관리자에게 문의하세요")
+      });
+    },
+    data(){
+      return{
+        commentList:{
+
+        },
+      }
     },
     data(){
       return{
